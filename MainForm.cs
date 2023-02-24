145a163581fc829c4da168823a7c22f2fa1dfb52
Bo Jordan <rojordan@microsoft.com>
2023-02-23 20:43:27 -0500

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics;
+
+namespace TimeToolbar
+{
+    public partial class MainForm : Form
+    {
+        private PerformanceCounter RamCounter { get; }
+        private PerformanceCounter CpuCounter { get; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.RamCounter = new PerformanceCounter("Memory", "Available MBytes", true);
+            this.CpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total", true);
+            
+            this.ForeColor = Color.Black;
+            this.BackColor = Color.FromArgb(213, 226, 239);
+            this.TransparencyKey = Color.FromArgb(213, 226, 239);
+            this.TopMost = true;
+
+            if (Screen.PrimaryScreen != null)
+            {
+                this.Location = new Point(5, Screen.PrimaryScreen.Bounds.Height - (this.Height));
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (!this.systemTrayNotifyIconMenuStrip.Visible)
+            {
+                this.TopMost = true;
+            }
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            this.label1.Text = $"{DateTime.UtcNow:t}";
+            this.label2.Text = "UTC";
+            this.label3.Text = $"{DateTime.Now.AddHours(-3):t}";
+            this.label4.Text = "Redmond";
+
+            //this.label1.BackColor = Color.Magenta;
+            //this.label2.BackColor = Color.Magenta;
+            //this.label3.BackColor = Color.CornflowerBlue;
+            //this.label4.BackColor = Color.CornflowerBlue;
+            //this.label5.BackColor = Color.OrangeRed;
+            //this.label6.BackColor = Color.OrangeRed;
+            //this.label7.BackColor = Color.YellowGreen;
+            //this.label8.BackColor = Color.YellowGreen;
+
+            this.label5.Text = "CPU";
+            this.label6.Text = "RAM";
+            this.label7.Text = $"{this.CpuCounter.NextValue():F0}%";
+            this.label8.Text = $"{1F - this.RamCounter.NextValue() / 1024F / 32F:P0}";
+        }
+
+        private void QuitMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# TimeToolbar
+A simple Windows system tray application and taskbar overlay to show times from multiple time zones.
+
+# License
+This application is available under the MIT License.
+
+# Icon
+Icon file: https://icon-icons.com/icon/clock/48329, from Cheat Sheet icons https://icon-icons.com/pack/Cheat-Sheet-icons/494
+Free version of https://linearicons.com/
+Created by https://perxis.com/
+Licensed under CC BY-SA 4.0 (Creative Commons Attribution-ShareAlike 4.0 International)
+https://creativecommons.org/licenses/by-sa/4.0/
